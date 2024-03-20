b8fe0b2de44cdf6d7be971edc8654b890f76419c
munazzabegam <4pa21ec037@pace.edu.in>
2024-03-21 00:48:40 +0530

    first commit

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mylinkedin(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    design_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    image_path VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+);
